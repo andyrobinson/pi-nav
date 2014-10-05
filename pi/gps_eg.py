97101a3bd3eaba85b383e352107f9c915c4d79d1
Andy Robinson <andy.robinson@thoughtworks.com>
2014-10-05 19:39:28 +0100

    Making it work with python 3

--- pi/gps_eg.py
+++ pi/gps_eg.py
@@ -35,23 +35,21 @@ if __name__ == '__main__':
  
       os.system('clear')
  
-      print
-      print ' GPS reading'
-      print '----------------------------------------'
-      print 'latitude    ' , gpsd.fix.latitude
-      print 'longitude   ' , gpsd.fix.longitude
-      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-      print 'altitude (m)' , gpsd.fix.altitude
-      print 'eps         ' , gpsd.fix.eps
-      print 'epx         ' , gpsd.fix.epx
-      print 'epv         ' , gpsd.fix.epv
-      print 'ept         ' , gpsd.fix.ept
-      print 'speed (m/s) ' , gpsd.fix.speed
-      print 'climb       ' , gpsd.fix.climb
-      print 'track       ' , gpsd.fix.track
-      print 'mode        ' , gpsd.fix.mode
-      print
-      print 'sats        ' , gpsd.satellites
+      print("\n GPS reading")
+      print("----------------------------------------")
+      print("latitude   " , gpsd.fix.latitude)
+      print("longitude  " , gpsd.fix.longitude)
+      print("time utc   " , gpsd.utc,' + ', gpsd.fix.time)
+      print("altitude (m)" , gpsd.fix.altitude)
+      print("eps        " , gpsd.fix.eps)
+      print("epx        " , gpsd.fix.epx)
+      print("epv        " , gpsd.fix.epv)
+      print("ept        " , gpsd.fix.ept)
+      print("speed (m/s)" , gpsd.fix.speed)
+      print("climb      " , gpsd.fix.climb)
+      print("track      " , gpsd.fix.track)
+      print("mode       " , gpsd.fix.mode)
+      print("\nsats       " , gpsd.satellites)
  
       time.sleep(5) #set to whatever
  
