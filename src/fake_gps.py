642657e28eb97484cf17c559b3ebc38194328794
Andy Robinson <arobinso@example.com>
2014-10-13 13:13:31 +0100

    reinstating FakeGPS and adding a simulate command, so that I can run a version of the app on systems without attached GPS

--- src/fake_gps.py
+++ src/fake_gps.py
@@ -1,4 +1,9 @@
+from position import Position
+
 class FakeGPS():
     def __init__(self):
-	    self.lat = 89.9
-	    self.long = 89.9
+        self.hasfix = True
+        self.position = Position(53.2,-2.3)
+        self.heading = 342.0
+        self.speed = 7.3
+        self.time = 10
