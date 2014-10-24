aae2befd944f36ebfddb89507debcf71c6202892
Andy Robinson <arobinso@example.com>
2014-10-24 11:03:19 +0100

    Adding exception handling for follower, so that it will log errors and continue; now need to limit the error rate in case of catasrophe

--- src/follower.py
+++ src/follower.py
@@ -1,3 +1,5 @@
+import sys
+
 class Follower():
     def __init__(self,navigator, logger):
         self.navigator = navigator
@@ -5,9 +7,16 @@ class Follower():
         
     def follow_route(self,waypoints):
         while waypoints:
-            next_waypoint = waypoints[0]
-            self.logger.info('Follower, next waypoint {:+f},{:+f}'.format(next_waypoint.longitude, next_waypoint.latitude))
-            self.navigator.to(next_waypoint)
-            waypoints = waypoints[1:]
-
+            try:        
+                next_waypoint = waypoints[0]
+                self.logger.info('Follower, next waypoint {:+f},{:+f}'.format(next_waypoint.longitude, next_waypoint.latitude))
+                self.navigator.to(next_waypoint)
+                waypoints = waypoints[1:]
+            except:
+                try:
+                    etype,e,traceback = sys.exc_info()
+                    self.logger.error('Follower, {0}: {1}'.format(etype.__name__,', '.join(str(x) for x in e.args)))
+                except:
+                    pass
+            
         self.logger.info('Follower, all waypoints reached, navigation complete')
--- test/test_follower.py
+++ test/test_follower.py
@@ -9,28 +9,71 @@ from follower import Follower
 from position import Position
 
 class TestFollower(unittest.TestCase):
+    
+    def setUp(self):
+        self.mock_navigator = Mock()
+        self.mock_logger = Mock()
+        self.follower = Follower(self.mock_navigator, self.mock_logger)
+    
     def test_should_navigate_along_list_of_waypoints_with_logging(self):
-        mock_navigator = Mock()
-        mock_logger = Mock()
         position1 = Position(11,11)
         position2 = Position(12,12)
 
-        navigator = Follower(mock_navigator, mock_logger)
-        navigator.follow_route([position1,position2])
+        self.follower.follow_route([position1,position2])
 
-        mock_navigator.to.has_calls([call(position1),call(position2)])
-        mock_logger.info.has_calls(
+        self.mock_navigator.to.has_calls([call(position1),call(position2)])
+        self.mock_logger.info.has_calls(
             [call('Follower, next waypoint {:+f},{:+f}'.format(position1.longitude, position1.latitude)),
              call('Follower, next waypoint {:+f},{:+f}'.format(position2.longitude, position2.latitude)),
              call('Follower, all waypoints reached, navigation complete')])
 
     def test_should_finish_navigation_and_log_on_empty(self):
-        mock_navigator = Mock()
+        self.follower.follow_route([])
+
+        self.mock_logger.info.assert_called_with('Follower, all waypoints reached, navigation complete')
+
+    def test_should_raise_an_error_from_a_mock(self):
+        mock = Mock(**{'throw.side_effect': KeyError})
+        self.assertRaises(KeyError, mock.throw)
+
+    def test_errors_should_be_logged_and_navigation_continues(self):
+        position1 = Position(11,11)
+        position2 = Position(12,12)
+
+        TestFollower.raise_error = True
+
+        def fail_first_time(self):
+            if TestFollower.raise_error:
+                TestFollower.raise_error = False
+                raise RuntimeError('oops')
+
+        mock_nav = Mock()
+        mock_nav.configure_mock(**{'to.side_effect': fail_first_time})
+
+        follower = Follower(mock_nav, self.mock_logger)
+
+        follower.follow_route([position1,position2])
+        self.mock_navigator.to.has_calls([call(position1),call(position2)])
+        self.mock_logger.error.assert_called_with('Follower, RuntimeError: oops')
+
+    def test_errors_during_error_logging_should_be_skipped_and_navigation_continues(self):
+        position1 = Position(11,11)
+        position2 = Position(12,12)
+
+        TestFollower.raise_error = True
+
+        def fail_first_time(self):
+            if TestFollower.raise_error:
+                TestFollower.raise_error = False
+                raise RuntimeError('oops')
+
+        mock_nav = Mock()
+        mock_nav.configure_mock(**{'to.side_effect': fail_first_time})
+
         mock_logger = Mock()
+        mock_logger.configure_mock(**{'error.side_effect': RuntimeError})
 
-        navigator = Follower(mock_navigator, mock_logger)
-        navigator.follow_route([])
+        follower = Follower(mock_nav, mock_logger)
 
-        mock_logger.info.assert_called_with('Follower, all waypoints reached, navigation complete')
-
-
+        follower.follow_route([position1,position2])
+        self.mock_navigator.to.has_calls([call(position1),call(position2)])
