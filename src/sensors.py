2051c201e464eda5d6d76f2a740acf24534303e5
Andy Robinson <andy.robinson@thoughtworks.com>
2014-11-01 16:29:05 +0000

    converting tabs to spaces - oops

--- src/sensors.py
+++ src/sensors.py
@@ -1,45 +1,44 @@
-
 from nan import isNaN
 from position import Position
 
 DEFAULT_ERROR = 20
 
 class Sensors():
-	def __init__(self,gps):
-		self.gps = gps
-		self._position = Position(gps.position.latitude,gps.position.longitude)
+    def __init__(self,gps):
+        self.gps = gps
+        self._position = Position(gps.position.latitude,gps.position.longitude)
 
-	@property
-	def hasfix(self):
-		return self.gps.hasfix
+    @property
+    def hasfix(self):
+        return self.gps.hasfix
 
-	@property
-	def position(self):
-		self._position.latitude = self.gps.position.latitude
-		self._position.longitude = self.gps.position.longitude
-		self._position.lat_error = self._default(self.gps.position.lat_error,DEFAULT_ERROR)
-		self._position.long_error = self._default(self.gps.position.long_error,DEFAULT_ERROR)
-		return self._position
+    @property
+    def position(self):
+        self._position.latitude = self.gps.position.latitude
+        self._position.longitude = self.gps.position.longitude
+        self._position.lat_error = self._default(self.gps.position.lat_error,DEFAULT_ERROR)
+        self._position.long_error = self._default(self.gps.position.long_error,DEFAULT_ERROR)
+        return self._position
 
-	@property
-	def time(self):
-		return self.gps.time
+    @property
+    def time(self):
+        return self.gps.time
 
-	@property
-	def track(self):
-		return self.gps.track
+    @property
+    def track(self):
+        return self.gps.track
 
-	@property
-	def speed(self):
-		return self.gps.speed	
+    @property
+    def speed(self):
+        return self.gps.speed   
 
-	@property
-	def track_error(self):
-		return self._default(self.gps.track_error,DEFAULT_ERROR)
-	
-	@property
-	def speed_error(self):
-		return self._default(self.gps.speed_error,DEFAULT_ERROR)
+    @property
+    def track_error(self):
+        return self._default(self.gps.track_error,DEFAULT_ERROR)
+    
+    @property
+    def speed_error(self):
+        return self._default(self.gps.speed_error,DEFAULT_ERROR)
 
-	def _default(self,	value,default):
-		return default if isNaN(value) else value
+    def _default(self,  value,default):
+        return default if isNaN(value) else value
