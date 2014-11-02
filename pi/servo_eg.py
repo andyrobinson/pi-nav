24c4ac26414cb37e7d293956bcf5f645c8836a3e
Andy <andy.robinson8@gmail.com>
2014-11-02 12:28:24 +0000

    Working servo example

--- pi/servo_eg.py
+++ pi/servo_eg.py
@@ -3,32 +3,30 @@
 import serial
 import time
 
-class ServoController:
-    def __init__(self):
-        usbPort = '/dev/ttyACM0'
-        self.sc = serial.Serial(usbPort)
+class Servo:
+    def __init__(self,port,channel,min_pulse,max_pulse,arc):
+        self.min_pulse = min_pulse
+        self.max_pulse = max_pulse
+        self.arc = arc
+        self.channel = channel
+        self.sc = serial.Serial(port)
 
     def closeServo(self):
         self.sc.close()
 
-    def setAngle(self, n, angle):
-        if angle > 180 or angle <0:
-           angle=90
-        byteone=int(254*angle/180)
-        bud=chr(0xFF)+chr(n)+chr(byteone)
-        self.sc.write(bud)
-
-    def setPosition(self, servo, position):
-        position = position * 4
+    def setPosition(self, angle):
+        if angle > self.arc or angle < 0:
+            angle=int(self.arc/2)
+        pulse_range = self.max_pulse-self.min_pulse
+        position = int(((float(angle)/self.arc) * pulse_range) + self.min_pulse) * 4
         poslo = (position & 0x7f)
         poshi = (position >> 7) & 0x7f
-        chan  = servo &0x7f
-        data =  chr(0xaa) + chr(0x0c) + chr(0x04) + chr(chan) + chr(poslo) + chr(poshi)
+        data =  chr(0xaa) + chr(0x0c) + chr(0x04) + chr(self.channel) + chr(poslo) + chr(poshi)
         self.sc.write(data)
 
-    def getPosition(self, servo):
-        chan  = servo &0x7f
-        data =  chr(0xaa) + chr(0x0c) + chr(0x10) + chr(chan)
+    def getPosition(self):
+        chan  = self.channel &0x7f
+        data =  chr(0xaa) + chr(0x0c) + chr(0x10) + chr(self.channel)
         self.sc.write(data)
         w1 = ord(self.sc.read())
         w2 = ord(self.sc.read())
@@ -41,39 +39,20 @@ class ServoController:
         w2 = ord(self.sc.read())
         return w1, w2
 
-    def triggerScript(self, subNumber):
-        data =  chr(0xaa) + chr(0x0c) + chr(0x21) + chr(0x27) + chr(subNumber)
-        self.sc.write(data)
 
 # MAIN
-         
-Maestro = ServoController()
 
-iterations = 2
+s = Servo('/dev/ttyACM0',0,500,2500,180)         
+val = '0'
 
-while(iterations > 0):
-    for chan in range(0,3):
-        for pos in range(0, 180):
-            Maestro.setAngle(chan, pos)
-            time.sleep(.002)
-            #print Maestro.getPosition(chan), Maestro.getErrors()
+while(val <> 'q'):
+    val = raw_input('enter angle (0-180), q to quit: ')
+    if val != 'q':
+        angle = int(val) 
+    else:
+        angle = 0
+    s.setPosition(angle)
+    time.sleep(0.1)
+    print 'position', s.getPosition(), s.getErrors()
 
-    for chan in range(0,3):
-        for pos in range(180, 0, -1):
-            Maestro.setAngle(chan, pos)
-            time.sleep(.002)
-            #print Maestro.getPosition(chan)
-
-    e1, e2 = Maestro.getErrors()
-    if e1 != 0 or e2 != 0:
-       print e1, e2
-       break
-
-    iterations = iterations - 1
-
-    print "Trigger Script"
-    Maestro.triggerScript(0)
-    
-time.sleep(2)
-Maestro.closeServo()
 
