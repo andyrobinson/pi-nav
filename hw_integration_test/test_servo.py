3228a059bfacf0bb5bdc1b361147ea504e4c1736
Andy Robinson <andy.robinson@thoughtworks.com>
2014-12-20 16:14:52 +0000

    final corrections to servo integration test

--- hw_integration_test/test_servo.py
+++ hw_integration_test/test_servo.py
@@ -13,7 +13,7 @@ class TestServo(unittest.TestCase):
     
     def set_and_read_position(self,position):
         self.servo.set_position(position)
-        time.sleep(1)
+        time.sleep(0.5)
         return self.servo.get_position()
 
     def setUp(self):
@@ -23,9 +23,9 @@ class TestServo(unittest.TestCase):
     def test_should_be_able_to_move_servo_and_read_position(self):
 
         for position in [-90,-45,5,45,90]:
-            self.assertLess(percentage_diff(self.set_and_read_position(position),position),1)
+            self.assertLess(percentage_diff(self.set_and_read_position(position),position),2)
 
     def test_for_errors(self):
-        a,b = self.servo.get_errors
+        a,b = self.servo.get_errors()
         self.assertEqual(a,0)
         self.assertEqual(b,0)
