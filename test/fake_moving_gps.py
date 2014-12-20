f7804cab7b6ed2f06833a217628b495e0949035b
Andy Robinson <andy.robinson@thoughtworks.com>
2014-12-20 12:55:40 +0000

    adding slightly dodgy integration test - fake_gps not sufficiently realistic

--- test/fake_moving_gps.py
+++ test/fake_moving_gps.py
@@ -1,13 +1,24 @@
+from globe import Globe
+
 class FakeMovingGPS():
-    def __init__(self,positions):
+    def __init__(self,positions,globe = Globe()):
         self.positions = positions
-        
-    def get_position(self):
-        value = self.positions[0]
-        if len(self.positions) > 1:
-            self.positions = self.positions[1:]
+        self.globe = globe
+        self.position_index = 0
+
+    @property        
+    def position(self):
+        value = self.positions[self.position_index]
+        if self.position_index < len(self.positions) - 1: 
+            self.position_index += 1
         return value
         
-    position = property(get_position)
+    @property
+    def track(self):
+        if self.position_index == 0:
+            return 0
+        else:
+            return self.globe.bearing(self.positions[self.position_index-1],self.positions[self.position_index])
+
     speed = 1
         
---
+++ test/integration/test_nav_and_helm.py
@@ -0,0 +1,70 @@
+from setup_test import setup_test
+setup_test()
+
+import unittest
+from mock import Mock, call
+
+import datetime
+from fake_moving_gps import FakeMovingGPS
+
+
+from navigator import Navigator
+from helm import Helm
+from waypoint import Waypoint
+from position import Position
+from globe import Globe
+from config import CONFIG
+
+def print_msg(msg):
+    print msg
+
+class TestNavigationAndHelm(unittest.TestCase):
+    
+    def setUp(self):
+        self.logger = Mock()
+        self.logger.error = Mock(side_effect=print_msg)
+        self.servo = Mock()
+    
+    def test_should_steer_to_next_waypoint(self):
+        destination = Waypoint(Position(10.03,10.03),10)
+        gps = FakeMovingGPS([Position(10,10),Position(10.01,10.01),Position(10.02,10.02),Position(10.03,10.03)])
+        helm = Helm(gps,self.servo,Mock(),self.logger, CONFIG['helm'])
+        navigator = Navigator(gps,helm,Globe(),self.logger, CONFIG['navigator'])
+
+        navigator.to(destination)
+
+        self.logger.info.assert_has_calls(
+            [call('Navigator, steering to +10.030000,+10.030000, bearing  44.6, distance 4681.8m'),
+            call('Navigator, steering to +10.030000,+10.030000, bearing  44.6, distance 3121.2m'),
+            call('Navigator, steering to +10.030000,+10.030000, bearing  44.6, distance 1560.6m'),
+            call('Navigator, arrived at +10.030000,+10.030000')])
+
+    def test_should_steer_to_next_waypoint_with_kink_in_route(self):
+        gps = FakeMovingGPS([Position(10,10),Position(10.01,10.01),Position(10.025,10.015),Position(10.03,10.03)])
+        helm = Helm(gps,self.servo,Mock(),self.logger, CONFIG['helm'])
+        navigator = Navigator(gps,helm,Globe(),self.logger, CONFIG['navigator'])
+        destination = Waypoint(Position(10.03,10.03),10)
+        navigator.to(destination)
+        self.logger.info.assert_has_calls(
+            [call('Navigator, steering to +10.030000,+10.030000, bearing  44.6, distance 4681.8m'),
+            call('Navigator, steering to +10.030000,+10.030000, bearing  44.6, distance 3121.2m'),
+            call('Navigator, steering to +10.030000,+10.030000, bearing  71.3, distance 1734.0m'),
+            call('Navigator, arrived at +10.030000,+10.030000')])
+
+    def test_should_steer_repeatedly_during_navigation(self):
+        destination = Waypoint(Position(10.0003,10.0003),10)
+        gps = FakeMovingGPS([Position(10,10),Position(10.0001,10.00015),Position(10.00025,10.0002),Position(10.0003,10.0003)])
+        helm = Helm(gps,self.servo,Mock(),self.logger, CONFIG['helm'])
+        navigator = Navigator(gps,helm,Globe(),self.logger, CONFIG['navigator'])
+
+        navigator.to(destination)
+
+        self.logger.debug.assert_has_calls(
+            [call('Helm, steering 44.6, tracking 55.9, rate of turn +55.9, rudder +0.0, new rudder -30.0'),
+            call('Helm, steering 44.6, tracking 55.9, rate of turn +0.0, rudder -30.0, new rudder -30.0'),
+            call('Helm, steering 44.6, tracking 55.9, rate of turn +0.0, rudder -30.0, new rudder -30.0'),
+            call('Helm, steering 44.6, tracking 55.9, rate of turn +0.0, rudder -30.0, new rudder -30.0'),
+            call('Helm, steering 44.6, tracking 55.9, rate of turn +0.0, rudder -30.0, new rudder -30.0'),
+            call('Helm, steering 44.6, tracking 55.9, rate of turn +0.0, rudder -30.0, new rudder -30.0'),
+            call('Helm, steering 44.6, tracking 55.9, rate of turn +0.0, rudder -30.0, new rudder -30.0')])
+
