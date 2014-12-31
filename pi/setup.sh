41ab1e710b8e76fdfc4c1b153a613cef9501d9cd
Andy Robinson <andy.robinson@thoughtworks.com>
2014-12-31 19:12:04 +0000

    Adding setup shell script for pi

---
+++ pi/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Install all required packages
+apt-get update
+apt-get install git gpsd gpsd-clients python-gps python-serial
+
+# Update boot settings
+mv /boot/cmdline.txt /boot/cmdlinetxt_$(date +"%Y%m%d").bak
+echo dwc_otg.lpm_enable=0console=tty1root=/dev/mmcblk0p2rootfstype=ext4elevator=deadlinerootwait > /boot/cmdline.txt
+
+# Disable tty listener in /etc/inittab
+# ADD COMMENT HASH TO THIS LINE - T0:23:respawn:/sbin/getty -L ttyAMA0 115200 vt100
+
+# setup GPSD and make it start on reboot
+mv /etc/default/gpsd /etc/default/gpsd__$(date +"%Y%m%d").bak
+cp -f ./etc/default/gpsd /etc/default/gpsd
+update-rc.d /etc/init.d/gpsd defaults 
+
+# Make the application run on startup
+mv /etc/rc.local /etc/rclocal__$(date +"%Y%m%d").bak
+cp -f ./etc/rc.local /etc
+
+sudo reboot
