7d45995e00f8a78bff8344b5d7c418ec175d4951
Andy Robinson <andy.robinson@thoughtworks.com>
2016-03-14 22:09:57 +0000

    setup shell - definitely a work in progress

--- pi/setup.sh
+++ pi/setup.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Install all required packages
-apt-get update
-apt-get install git gpsd gpsd-clients python-gps python-serial
+This is a bit risky - apt-get update
+apt-get install gpsd gpsd-clients python-gps python-serial
 
 # Update boot settings
 mv /boot/cmdline.txt /boot/cmdlinetxt_$(date +"%Y%m%d").bak
-echo dwc_otg.lpm_enable=0console=tty1root=/dev/mmcblk0p2rootfstype=ext4elevator=deadlinerootwait > /boot/cmdline.txt
+echo dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait > /boot/cmdline.txt
 
 # Disable tty listener in /etc/inittab, bit crude, stop double # comments after
 sed -e 's/T0/#T0/;s/##/#/' -i.bak /etc/inittab
