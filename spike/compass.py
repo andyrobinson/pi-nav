5af40e5bd53e27df811d77833031ea9b4a8bd81b
Andy Robinson <andy.robinson8@gmail.com>
2016-05-01 20:28:30 +0100

    compass spike with accelerometer, some output data

--- spike/compass.py
+++ spike/compass.py
@@ -30,6 +30,9 @@ class I2C:
         else:
             return val
 
+    def read12_2s_comp(self,register):
+        return self.read16_2s_comp(register,False) >> 4 
+
     def write8(self,register, value):
         self.bus.write_byte_data(self.address, register, value)
 
@@ -40,14 +43,17 @@ compass.write8(2, 0b00000000) # Continuous sampling
 
 accel = I2C(0x19)
 accel.write8(0x20,0b00110111) # Enable accelerometer, 25Hz, normal mode, all axes enabled
-accel.write8(0x23,0b00000000) # Low res (10 bit) mode, LSB at lower address, default serial interface
+accel.write8(0x23,0b00001000) # High res (12 bit) mode, LSB at lower address, default serial interface
 
 def print_accel():
-    x_acc = accel.read16_2s_comp(0x28,False) >> 4
-    y_acc = accel.read16_2s_comp(0x2a,False) >> 4
-    z_acc = accel.read16_2s_comp(0x2c,False) >> 4
+    x_acc = accel.read12_2s_comp(0x28)
+    y_acc = accel.read12_2s_comp(0x2a)
+    z_acc = accel.read12_2s_comp(0x2c)
     print (x_acc, y_acc, z_acc)
 
+    pitch = round(math.degrees(math.atan2(x_acc,z_acc)),0)
+    roll = round(math.degrees(math.atan2(y_acc,z_acc)),0)
+    print ("pitch (nose +up,-down), roll(+R,-L): {0},{1}".format(pitch,roll))
 
 def print_compass():
     x_out = compass.read16_2s_comp(3) 
@@ -64,11 +70,24 @@ def print_compass():
     #print x_adj,",",y_adj,",",z_out
     #print x_out,",",y_out,",",z_out
 
+
+def print_both():
+    x_acc = accel.read12_2s_comp(0x28)
+    y_acc = accel.read12_2s_comp(0x2a)
+    z_acc = accel.read12_2s_comp(0x2c)
+
+    x_out = compass.read16_2s_comp(3) 
+    y_out = compass.read16_2s_comp(7) 
+    z_out = compass.read16_2s_comp(5)
+
+    print("{0},{1},{2},{3},{4},{5}".format(x_out,y_out,z_out,x_acc,y_acc,z_acc))
+
 while True:
     try:
         #print_compass()
-        print_accel()
-        time.sleep(0.5)
+        #print_accel()
+        print_both()
+        time.sleep(0.1)
     except(KeyboardInterrupt):
         quit()
 
