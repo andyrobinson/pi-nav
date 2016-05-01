1661e1bba6469cd1f16470a14fc2ae69d9324841
Andy Robinson <andy.robinson8@gmail.com>
2016-05-01 12:50:06 +0100

    tidying up compass spike ahead of accel work

--- spike/compass.py
+++ spike/compass.py
@@ -6,45 +6,54 @@ import smbus
 import time
 import math
 
-bus = smbus.SMBus(1)
-address = 0x1e
+class I2C:
+    def __init__(self,address):
+        self.address = address
+        self.bus = smbus.SMBus(1)
 
+    def read8(self,register):
+        return self.bus.read_byte_data(self.address, register)
 
-def read_byte(adr):
-    return bus.read_byte_data(address, adr)
-
-def read_word(adr):
-    high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr+1)
-    val = (high << 8) + low
-    return val
-
-def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
+    def read16(self, register):
+        high = self.bus.read_byte_data(self.address, register)
+        low = self.bus.read_byte_data(self.address, register+1)
+        val = (high << 8) + low
         return val
 
-def write_byte(adr, value):
-    bus.write_byte_data(address, adr, value)
+    def read16_2s_comp(self,register):
+        val = self.read16(register)
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
 
-write_byte(0, 0b01110000) # Set to 8 samples @ 15Hz
-write_byte(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
-write_byte(2, 0b00000000) # Continuous sampling
+    def write8(self,register, value):
+        self.bus.write_byte_data(self.address, register, value)
+
+compass = I2C(0x1e)
+compass.write8(0, 0b01110000) # Set to 8 samples @ 15Hz
+compass.write8(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
+compass.write8(2, 0b00000000) # Continuous sampling
+
+def print_compass():
+    x_out = compass.read16_2s_comp(3) 
+    y_out = compass.read16_2s_comp(7) 
+    z_out = compass.read16_2s_comp(5)
+    #y_adj = y_out*0.95 + 13
+    y_adj = y_out
+    #x_adj = x_out - 23
+    x_adj = x_out
+    bearing  = math.atan2(y_adj, x_adj) 
+    if (bearing < 0):
+        bearing += 2 * math.pi
+    print x_adj,y_adj,z_out, " bearing: ", round(math.degrees(bearing),0)
+    #print x_adj,",",y_adj,",",z_out
+    #print x_out,",",y_out,",",z_out
 
 while True:
-    try:     
-        x_out = read_word_2c(3) * 1.01
-        y_out = read_word_2c(7) * 0.9
-        z_out = read_word_2c(5)
-        y_adj = y_out * 1.1 + 335
-        x_adj = x_out - 215
-        bearing  = math.atan2(y_adj, x_adj) 
-        if (bearing < 0):
-            bearing += 2 * math.pi
-        print x_adj,y_adj,z_out, " bearing: ", math.degrees(bearing)
-        #print x_adj,",",y_adj
+    try:
+        print_compass()
         time.sleep(0.1)
     except(KeyboardInterrupt):
         quit()
+
