4c23801e84f1c245dbd49240d345c0540d8f8f1f
Andy Robinson <andy.robinson8@gmail.com>
2016-05-08 21:35:23 +0100

    Adding delayed event firing class

--- src/timeshift.py
+++ src/timeshift.py
@@ -2,5 +2,19 @@ from events import Event,EventName
 
 class TimeShift:
 
-  def __init__(self,exchange):
-    self.exchange = exchange
+    def __init__(self,exchange,time):
+        self.exchange = exchange
+        self.pending_events = {}
+        self.time = time
+        exchange.subscribe(EventName.after,self._register_event)
+        exchange.subscribe(EventName.tick,self._fire_past_events)
+
+    def _fire_past_events(self,tick_event):
+        current_time = self.time()
+        events_to_fire = dict((time_to_fire, event) for time_to_fire, event in self.pending_events.iteritems() if time_to_fire < current_time)
+        for time_to_fire,event in events_to_fire.iteritems():
+            self.exchange.publish(event)
+            del self.pending_events[time_to_fire]
+
+    def _register_event(self,after_event):
+        self.pending_events[self.time() + after_event.seconds] = after_event.next_event
--- test/unit/test_timeshift.py
+++ test/unit/test_timeshift.py
@@ -31,14 +31,55 @@ class EventTest(unittest.TestCase):
 
 class TestTimeShift(EventTest):
 
-  def setUp(self):
-      super(TestTimeShift, self).setUp()
+    def setUp(self):
+        super(TestTimeShift, self).setUp()
 
-  def test_should_fire_event_past_its_delay_on_tick(self):
-    self.listen(EventName.start)
-    timeshift = TimeShift(self.exchange)
+    def test_should_not_fire_event_on_tick_if_its_not_yet_time(self):
+        self.listen(EventName.start)
+        mock_time = Mock(side_effect=[145.6, 146.4])
+        timeshift = TimeShift(self.exchange,mock_time)
 
-    self.exchange.publish(Event(EventName.after,seconds = 0,next_event=Event(EventName.start)))
-    self.exchange.publish(Event(EventName.tick))
+        self.exchange.publish(Event(EventName.after,seconds = 3,next_event=Event(EventName.start)))
+        self.exchange.publish(Event(EventName.tick))
+        self.assertEqual(self.event_count(EventName.start),0,"start should not have been called")
 
-    self.assertEqual(self.last_event.name,EventName.start)
+    def test_should_fire_event_on_tick_if_its_past_its_time(self):
+        self.listen(EventName.start)
+        mock_time = Mock(side_effect=[145.6, 155.4])
+        timeshift = TimeShift(self.exchange,mock_time)
+
+        self.exchange.publish(Event(EventName.after,seconds = 5,next_event=Event(EventName.start)))
+        self.exchange.publish(Event(EventName.tick))
+
+        self.assertEqual(self.event_count(EventName.start),1,"start should have been called")
+        self.assertEqual(self.last_event.name,EventName.start)
+
+    def test_should_only_fire_events_once(self):
+        self.listen(EventName.start)
+        mock_time = Mock(side_effect=[145.6, 155.4, 166.2])
+        timeshift = TimeShift(self.exchange,mock_time)
+
+        self.exchange.publish(Event(EventName.after,seconds = 5,next_event=Event(EventName.start)))
+        self.exchange.publish(Event(EventName.tick))
+        self.exchange.publish(Event(EventName.tick))
+
+        event_count = self.event_count(EventName.start)
+        self.assertEqual(event_count,1,"start should have been called once, but was called {0} times".format(event_count))
+
+    def test_should_be_able_to_keep_and_fire_multiple_events(self):
+        self.listen(EventName.start)
+        self.listen(EventName.navigate)
+
+        # Note that time() is called on register, then on tick, awkwardness only during test
+        mock_time = Mock(side_effect=[145.6, 145.6, 155.4, 166.2])
+        timeshift = TimeShift(self.exchange,mock_time)
+
+        self.exchange.publish(Event(EventName.after,seconds = 5,next_event=Event(EventName.start)))
+        self.exchange.publish(Event(EventName.after,seconds = 10,next_event=Event(EventName.navigate)))
+
+        self.exchange.publish(Event(EventName.tick))
+        self.assertEqual(self.event_count(EventName.start),1)
+        self.assertEqual(self.event_count(EventName.navigate),0)
+
+        self.exchange.publish(Event(EventName.tick))
+        self.assertEqual(self.event_count(EventName.navigate),1)
