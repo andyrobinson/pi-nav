61b3721d1e80b25d4e28f32da1ad26a7d24d8843
Andy Robinson <andy.robinson8@gmail.com>
2016-05-09 21:45:00 +0100

    extracting Test helper for testing classes that use events - needs using everywhere

--- test/unit/test_timeshift.py
+++ test/unit/test_timeshift.py
@@ -2,34 +2,12 @@ from setup_test import setup_test
 setup_test()
 import unittest
 from mock import Mock, call
-import logging
 
 from timeshift import TimeShift
 from events import Exchange,EventName,Event
+from test_utils import EventTestCase
 
-class EventTest(unittest.TestCase):
-
-    def setUp(self):
-        self.last_event = Event("None")
-        self.events = {}
-        logging.basicConfig(format='%(asctime)s,%(levelname)s,%(message)s', level=logging.ERROR)
-        self.exchange = Exchange(logging.getLogger("test"))
-
-    def event_recorder(self,event):
-        self.last_event = event
-        self.events[event.name].append(event)
-
-    def listen(self,event_name):
-        self.events[event_name] = []
-        self.exchange.subscribe(event_name,self.event_recorder)
-
-    def event_count(self,event_name):
-        if event_name in self.events:
-            return len(self.events[event_name])
-        else:
-            return 0
-
-class TestTimeShift(EventTest):
+class TestTimeShift(EventTestCase):
 
     def setUp(self):
         super(TestTimeShift, self).setUp()
--- test/utils/test_utils.py
+++ test/utils/test_utils.py
@@ -1,3 +1,30 @@
-def percentage_diff(original,to_compare):
-    return abs(to_compare-original)*100/abs(original)    
+from setup_test import setup_test
+setup_test()
+import unittest
+import logging
+from events import Exchange,EventName,Event
 
+def percentage_diff(original,to_compare):
+    return abs(to_compare-original)*100/abs(original)
+
+class EventTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.last_event = Event("None")
+        self.events = {}
+        logging.basicConfig(format='%(asctime)s,%(levelname)s,%(message)s', level=logging.ERROR)
+        self.exchange = Exchange(logging.getLogger("test"))
+
+    def event_recorder(self,event):
+        self.last_event = event
+        self.events[event.name].append(event)
+
+    def listen(self,event_name):
+        self.events[event_name] = []
+        self.exchange.subscribe(event_name,self.event_recorder)
+
+    def event_count(self,event_name):
+        if event_name in self.events:
+            return len(self.events[event_name])
+        else:
+            return 0
