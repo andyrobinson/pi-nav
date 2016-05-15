afac838dd1a164adc2ba06277a300a23c246b645
Andy Robinson <andy.robinson8@gmail.com>
2016-05-15 16:04:45 +0100

    Improving readability of windsensor

--- src/windsensor.py
+++ src/windsensor.py
@@ -1,4 +1,4 @@
-MAX_RAW = 0x3FFF
+MAX_RAW_14_BITS = 0x3FFF
 
 class WindSensor:
 
@@ -7,8 +7,14 @@ class WindSensor:
 
     @property
     def angle(self):
-        high = self.i2c.read8(0xFF)
-        low = self.i2c.read8(0xFE)
-        raw_value = (high << 6) + (low & 0x3F)
-        degrees = round(360 * (raw_value/float(MAX_RAW)),0) % 360
+        high8bits = self.i2c.read8(0xFF)
+        low6bits = self.i2c.read8(0xFE)
+        raw_value_14_bits = self._shift_left_6_bits(high8bits) + self._lowest_6_bits(low6bits)
+        degrees = round(360 * (raw_value_14_bits/float(MAX_RAW_14_BITS)),0) % 360
         return degrees
+
+    def _lowest_6_bits(self,byte):
+        return byte & 0x3F
+
+    def _shift_left_6_bits(self,word):
+        return word << 6
