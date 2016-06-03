386c70210aa8397f2f3b4de03634cdb1717eda9f
Andy Robinson <andy.robinson8@gmail.com>
2016-06-03 15:57:53 +0100

    tidying up nav and helm integration test

--- test/integration/test_nav_and_helm.py
+++ test/integration/test_nav_and_helm.py
@@ -5,6 +5,7 @@ import logging
 import unittest
 from mock import Mock, call
 
+from bearing import angle_between,to_360
 from fake_moving_gps import FakeMovingGPS
 from navigator import Navigator
 from helm import Helm
@@ -22,7 +23,39 @@ from test_utils import test_logger
 def print_msg(msg):
     print msg
 
+
+class FakeSensors:
+
+    def __init__(self,gps,wind_direction,compass_heading):
+        self.gps = gps
+        self.time = 0
+        self.speed = 1
+        self.track_error = 1
+        self.speed_error = 0.1
+        self.wind_direction_abs_average = wind_direction
+        self.wind_direction_relative_instant = to_360(angle_between(wind_direction,compass_heading))
+        self.wind_direction_relative_average = self.wind_direction_relative_instant
+        self.compass_heading_average = compass_heading
+        self.compass_heading_instant = compass_heading
+
+    @property
+    def hasfix(self):
+        return self.gps.hasfix
+
+    @property
+    def position(self):
+        return self.gps.position
+
+    @property
+    def track(self):
+        return self.gps.track
+
 class TestNavigationAndHelm(unittest.TestCase):
+
+    def ticks(self,number,duration):
+        for i in range(1,number):
+            self.exchange.publish(Event(EventName.tick))
+            self.timer.wait_for(duration)
 
     def setUp(self):
         self.logger = Mock()
@@ -35,21 +68,13 @@ class TestNavigationAndHelm(unittest.TestCase):
 
     def test_should_steer_to_next_waypoint(self):
         destination = Waypoint(Position(10.03,10.03),10)
-        gps = FakeMovingGPS([Position(10,10),Position(10,10),Position(10.01,10.01),Position(10.02,10.02),Position(10.03,10.03)])
-        windsensor = Mock()
-        windsensor.angle.side_effect = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-        compass = Mock()
-        compass.bearing.side_effect = [45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45]
-
-        sensors = Sensors(gps,windsensor,compass,self.exchange,CONFIG['sensors'])
+        gps = FakeMovingGPS([Position(10,10),Position(10.01,10.01),Position(10.02,10.02),Position(10.03,10.03)])
+        sensors = FakeSensors(gps,1,45)
         helm = Helm(self.exchange,sensors,self.servo,self.logger, CONFIG['helm'])
         navigator = Navigator(sensors,Globe(),self.exchange,self.logger, CONFIG['navigator'])
 
         self.exchange.publish(Event(EventName.navigate,waypoint = destination))
-
-        for i in range(1,14):
-            self.exchange.publish(Event(EventName.tick))
-            self.timer.wait_for(200)
+        self.ticks(number = 14,duration=200)
 
         self.logger.info.assert_has_calls(
             [call('Navigator, steering to +10.030000,+10.030000, bearing  44.6, distance 4681.8m'),
