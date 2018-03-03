ab4afd5c84a221ba2096c3c76ad21d5cd0481e9b
Andy Robinson <andy.robinson01@bbc.co.uk>
2018-03-03 23:05:47 +0000

    Test kind of working (rate of turn zero, still high rudder deflection at completion

--- test/integration/test_helm_oscillation.py
+++ test/integration/test_helm_oscillation.py
@@ -8,6 +8,7 @@ from test_utils import EventTestCase
 from nan import NaN
 from utils.stub_gps import StubGPS
 from events import Event, EventName
+from bearing import to_360
 
 from sensors import Sensors
 from helm import Helm
@@ -23,7 +24,6 @@ class RudderSimulator:
 
   def set_position(self,angle):
     self.angle = angle
-    print 'angle: ' + str(angle)
 
   def get_position(self):
     return self.angle
@@ -56,37 +56,31 @@ class TestHelmOscillation(EventTestCase):
     self.rudder_servo = RudderSimulator()
     self.steerer = Steerer(self.rudder_servo,self.logger,STEERER_CONFIG)
     self.helm = Helm(self.exchange, self.sensors,self.steerer,self.logger, HELM_CONFIG)
-    self.helm.previous_heading = 180
+    self.helm.previous_heading = 0
 
   def deviation(self,heading):
     return abs(self.sensors.compass_heading_average-heading)
 
-  def currently_tracking(self,previous_heading, current_track, rudder_angle=0):
-    self.sensors.compass_heading_smoothed = current_track
-    self.sensors.compass_heading_average = NaN
-    self.sensors.rate_of_turn = current_track - previous_heading
-    self.sensors.rate_of_turn_average = self.sensors.rate_of_turn
+  def rotate_boat(self,rudder_effect):
+    self.compass.bearing = to_360(self.compass.bearing - self.rudder_servo.get_position() * rudder_effect)
+    self.exchange.publish(Event(EventName.tick)) # for compass smoothing!
+    self.exchange.publish(Event(EventName.update_averages))
 
   def test_it_should_converge_on_requested_heading(self):
     target_heading = 90
     previous_deviation = 90
-    self.currently_tracking(180,180)
-    self.sensors.compass_heading_average = 180
 
     self.sensors.log_values(Event(EventName.log_position))
-
     self.exchange.publish(Event(EventName.set_course,heading=target_heading))
 
     while (self.deviation(target_heading) > STEERER_CONFIG['ignore deviation below']):
-        if (self.rudder_servo.get_position <> 0):
-          self.compass.bearing = self.compass.bearing + self.rudder_servo.get_position() * 0.5
-          self.exchange.publish(Event(EventName.tick)) # for compass smoothing!
-          self.exchange.publish(Event(EventName.update_averages))
+      if (self.rudder_servo.get_position <> 0):
+        self.rotate_boat(0.5)
 
-        self.sensors.log_values(Event(EventName.log_position))
-        self.exchange.publish(Event(EventName.steer))
-        self.assertGreater(previous_deviation, self.deviation(target_heading))
-        previous_deviation = self.deviation(target_heading)
+      self.sensors.log_values(Event(EventName.log_position))
+      self.exchange.publish(Event(EventName.steer))
+      self.assertGreater(previous_deviation, self.deviation(target_heading))
+      previous_deviation = self.deviation(target_heading)
 
   def ignore_it_should_converge_on_requested_heading_when_rudder_highly_effective(self):
     pass
